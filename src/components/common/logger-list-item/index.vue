0cc711014798e6dab1edbd9c314c380f44f04d1d
zhanjun <nzj19910102@163.com>
2017-12-19 17:57:51 +0800

    fix bug

--- src/components/common/logger-list-item/index.vue
+++ src/components/common/logger-list-item/index.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="logger-list-item">
+        <div class="logger-list-user">
+            <div class="logger-list-row">
+                <span class="label">{{loggerItemData.userName}}</span>
+                <span class="caption">{{loggerItemData.createTime | filterDiaryUserTime}}</span>
+            </div>
+        </div>
         <div class="logger-list-range ellipsis">
-            <span v-for="(item, index) in loggerItemData.range" :key="index">
+            <span class="caption" v-for="(item, index) in loggerItemData.range" :key="index">
                 {{item.teamName || item.spaceName}}
             </span>
         </div>
@@ -13,15 +19,15 @@
                 {{dataSource[loggerItemData.dataType || 0]}}
             </span>
         </div>
-        <div class="logger-list-time">
-            <span>日志日期</span>
-            <span>{{loggerItemData.diaryTime | filterDiaryTime}}</span>
+        <div class="logger-list-time logger-list-row">
+            <span class="label">日志日期</span>
+            <span class="caption">{{loggerItemData.diaryTime | filterDiaryTime}}</span>
         </div>
-        <div class="logger-list-content"
+        <div class="logger-list-content logger-list-row"
             v-for="(item, index) in JSON.parse(loggerItemData.content)"
             :key="index">
-            <span class="logger-list-content-title">{{item.title}}</span>
-            <span class="logger-list-content-value">{{item.value || item.content}}</span>
+            <span class="label">{{item.title}}</span>
+            <span class="caption">{{item.value || item.content}}</span>
         </div>
         <div class="logger-list-operate">
 
@@ -44,6 +50,9 @@ export default {
     filters: {
         filterDiaryTime(val) { // 格式化日志日期
             return FormatTime(new Date(val), 'YYYY-MM-DD')
+        },
+        filterDiaryUserTime(val) { // 格式化日志日期
+            return FormatTime(new Date(val), 'MM-DD HH:mm')
         }
     },
     methods: {
@@ -58,9 +67,46 @@ export default {
 <style lang="less" scoped>
 @import '../../../assets/css/var.less';
 .logger-list-item {
+    padding: 14px 30px;
+    transition: .4s ease all;
+    color: rgb(130, 130, 130);
+    &>div {
+        margin-bottom: 10px;
+    }
+    .logger-list-row {
+        line-height: 1.5;
+        span {
+            display: block;
+            font-size: 14px;
+        }
+        .label {
+            color: @gray-color-dark;
+        }
+        .caption {
+            color: @gray-color-light;
+        }
+    }
+
+    .logger-list-user {
+        .caption {
+            font-size: 12px;
+        }
+    }
     .logger-list-range {
         color: @gray-color-light;
         font-size: 12px;
+        padding: 1px 0;
+    }
+    .logger-list-tips {
+        color: #fff;
+        font-size: 12px;
+        span {
+            border-radius: 2px;
+            margin-right: 5px;
+            padding: 3px 4px;
+            display: inline-block;
+            background-color: @primary-color;
+        }
     }
     &:hover {
         background-color: @white-color-light;
