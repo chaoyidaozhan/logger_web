afd70b50c02a700fb9954e46be9c25097c5c005f
chenawen <caw736639943@gmail.com>
2018-01-03 21:05:00 +0800

    chenawen_回复

---
+++ src/components/common/reply/image.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="img-list">
+        <div class="img-item" 
+            v-for="(item, index) in images" :key="index">
+            <img :src="item.fileUrl" :alt="item.fileName">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        images: {
+            type: Array,
+            default: []
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .img-list {
+        .img-item {
+            float: left;
+            height: 110px;
+            width: 110px;
+            margin-right: 10px;
+            margin-bottom: 10px;
+            cursor: pointer;
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+</style>
