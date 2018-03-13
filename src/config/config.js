2ddc518f2b2411d5483d156b371bd57f6f115b17
zhanjun <nzj19910102@163.com>
2018-03-13 15:58:04 +0800

    fix config

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     token: 'd12fdcc7-b9ac-48c4-adf6-5b1958e9afc4',
     development: { // 开发环境
         host: '//123.103.9.204:93',
-        apiHost: '//123.103.9.204:6058/logger_new',
+        apiHost: '//123.103.9.204:6058/logger',
     },
     prevdev: { // 预发布开发环境
         apiHost: '//ezone.esn.ren/logger',
@@ -13,7 +13,7 @@ module.exports = {
         apiHost: 'https://ezone.yonyoucloud.com/logger'
     },
     test: {
-        apiHost: '//123.103.9.204:6058/logger_new',
+        apiHost: '//123.103.9.204:6058/logger',
     },
     prev: {
         apiHost: '//ezone.esn.ren/logger',
