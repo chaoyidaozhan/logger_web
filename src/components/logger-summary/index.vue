82c69532ddd614abd4364c81be0445baeadebf46
chenawen <chenaw@yonyou.com>
2018-07-18 14:26:28 +0800

    fix summary export bug

--- src/components/logger-summary/index.vue
+++ src/components/logger-summary/index.vue
@@ -132,7 +132,7 @@ export default {
         exportECL() { // 导出
             let templateId = this.params.templateId == null ? 0 : this.params.templateId;
             let url = `${config[__ENV__].apiHost}/diaryQuery/exportDiaryStatistics?timestamp=${(new Date()).valueOf()}&beginDate=${this.params.beginDate}
-            &endDate=${this.params.endDate}&token=${this.$store.state.userInfo.token}&templateId=${templateId}` 
+            &endDate=${this.params.endDate}&token=${this.$store.state.userInfo.token}&templateId=${templateId}&memberIds=${this.params.memberIds || ""}&groupIds=${this.params.groupId || ""}&deptIds=${this.params.deptId || ""}`;
             window.open(url, '_blank');
         },
         updateList(res) { // 更新列表
