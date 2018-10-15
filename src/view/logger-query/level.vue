035f645caa3960821cb144da23b41f0ed2f80e41
zhanjun <nzj19910102@163.com>
2018-10-15 15:31:42 +0800

    fix url bug

--- src/view/logger-query/level.vue
+++ src/view/logger-query/level.vue
@@ -5,6 +5,7 @@
                 :showTemplate="true"
                 :showDatePicker="true"
                 :showDept="true"
+                deptApiUri="/team/getDeptsWithPart"
                 @handleQuery="handleQuery" 
                 ref="queryForm"/>
         </template>
