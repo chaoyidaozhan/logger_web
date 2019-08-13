98d7ff4ce3908ce81aa542dc16cccf59b52a83f7
nizhja <nzj19910102@163.com>
2019-08-13 17:04:12 +0800

    fix bug

--- src/config/config.js
+++ src/config/config.js
@@ -10,14 +10,22 @@ const YKJ_IS_DIWORK = getCookie('YKJ_IS_DIWORK')
 module.exports = {
     development: { // 开发环境
         uploadHost: '//web.api.chaoke.com:6062',
-        host: '//ezone.chaoke.com:6058/logger',
+        apiHost: '//ezone.chaoke.com:6058/logger',
     },
     develop: { // 91
         uploadHost: '//web.api.chaoke.com:6062',
-        host: '//ezone.chaoke.com:6058/logger',
+        apiHost: '//ezone.chaoke.com:6058/logger',
+    },
+    test: { // daily
+        uploadHost: '//DWWEB-API',
+        apiHost: '//EZONE/logger',
+    },
+    prev: {
+        uploadHost: '//web-api.esn.ren',
+        apiHost: '//ezone.esn.ren/logger'
     },
     production: { // 生产环境
-        uploadHost: `//${YKJ_IS_DIWORK === '1' ? 'dw' : ''}WEBAPI`,
-        host: '//EZONE/logger'
+        uploadHost: `//${YKJ_IS_DIWORK === '1' ? 'dw' : ''}web-api.diwork.com`,
+        apiHost: '//ezone.diwork.com/logger'
     }
 }
