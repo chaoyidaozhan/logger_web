985b734a30051df22e0ccd939d78bcbdade81a6c
nizhja <nzj19910102@163.com>
2019-08-13 16:59:52 +0800

    fix bug

--- src/config/config.js
+++ src/config/config.js
@@ -8,31 +8,16 @@ function getCookie(name) {
 }
 const YKJ_IS_DIWORK = getCookie('YKJ_IS_DIWORK')
 module.exports = {
-    v: '4.5',
-    salt: 'BAN/+GGzUBtMW',
-    token: 'd12fdcc7-b9ac-48c4-adf6-5b1958e9afc4',
-    'development': { // 开发环境
+    development: { // 开发环境
         uploadHost: '//web.api.chaoke.com:6062',
-        apiHost: '//ezone.chaoke.com:6058/logger',
+        host: '//ezone.chaoke.com:6058/logger',
     },
-    'develop': { // 91
+    develop: { // 91
         uploadHost: '//web.api.chaoke.com:6062',
-        apiHost: '//ezone.chaoke.com:6058/logger',
+        host: '//ezone.chaoke.com:6058/logger',
     },
-    'test': { // daily
-        uploadHost: '//dwweb-api.yyuap.com',
-        apiHost: '//ezone-daily.yyuap.com/logger',
-    },
-    'u8c_daily': { // u8c_daliy
-        uploadHost: '//dwweb-api-u8c-daily.yyuap.com',
-        apiHost: '//ezone-u8c-daily.yyuap/logger',
-    },
-    'prev': {
-        uploadHost: '//web-api.esn.ren',
-        apiHost: '//ezone.esn.ren/logger'
-    },
-    'production': { // 生产环境
-        uploadHost: `//${YKJ_IS_DIWORK === '1' ? 'dw' : ''}web-api.diwork.com`,
-        apiHost: '//ezone.diwork.com/logger'
+    production: { // 生产环境
+        uploadHost: `//${YKJ_IS_DIWORK === '1' ? 'dw' : ''}WEBAPI`,
+        host: '//EZONE/logger'
     }
 }
