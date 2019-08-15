6dad789c9569da1d386013cc0c8236880107954b
nizhja <nzj19910102@163.com>
2019-08-15 13:15:30 +0800

    chore: 更新构建指令

--- src/config/config.js
+++ src/config/config.js
@@ -10,22 +10,22 @@ const YKJ_IS_DIWORK = getCookie('YKJ_IS_DIWORK')
 module.exports = {
     dev: { // 开发环境
         uploadHost: '//web.api.chaoke.com:6062',
-        host: '//ezone.chaoke.com:6058/logger',
+        host: '//ezone.chaoke.com:6058',
     },
     develop: { // 91
         uploadHost: '//web.api.chaoke.com:6062',
-        host: '//ezone.chaoke.com:6058/logger',
+        host: '//ezone.chaoke.com:6058',
     },
     test: { // daily
-        uploadHost: '//DWWEB',
-        host: '//EZONE/logger',
+        uploadHost: '//DWWEBAPI',
+        host: '//EZONE',
     },
     prev: {
         uploadHost: '//web-api.esn.ren',
-        host: '//ezone.esn.ren/logger'
+        host: '//ezone.esn.ren'
     },
     build: { // 生产环境
         uploadHost: `//${YKJ_IS_DIWORK === '1' ? 'dw' : ''}web-api.diwork.com`,
-        host: '//ezone.diwork.com/logger'
+        host: '//ezone.diwork.com'
     }
 }
