7dc7d4f61dc8ea70743ff13da60ceca989d46c58
dingyuc <dingyuc@yonyou.com>
2020-03-12 15:37:13 +0800

    2020-03-12 15:37

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import { getWebLang } from 'yyzone/src/utils/lang'
 import FsVueVideo from './components/common/video/'
 import selectTree from './components/common/select-tree'
 import 'app_src/directives/loading/'
+import yyzone from 'yyzone'
 
 // css文件
 import 'video.js/dist/video-js.min.css'
@@ -23,26 +24,6 @@ import 'yyzone/dist/styles/yyzone.min.css'
 import './assets/css/common.less'
 import 'vue-photo-preview/dist/skin.css'
 import './assets/css/flex.less'
-
-// 按需引入YYZone组件
-import YYAvatar from 'yyzone/src/components/base/avatar/'
-import YYButton from 'yyzone/src/components/base/button/'
-import YYEmpty from 'yyzone/src/components/base/empty/'
-import YYRadio from 'yyzone/src/components/base/radio/'
-import YYCheckbox from 'yyzone/src/components/base/checkbox/'
-import YYDatePicker from 'yyzone/src/components/base/date-picker/'
-import YYSelect from 'yyzone/src/components/base/select/'
-import YYPagination from 'yyzone/src/components/base/pagination/'
-import YYLoading from 'yyzone/src/components/base/loading/'
-import YYLoadingDirective from 'yyzone/src/directives/loading'
-import YYMessage from 'yyzone/src/components/base/message/'
-import YYLoadingH from 'yyzone/src/components/base/loading-h/'
-import YYModal from 'yyzone/src/components/base/modal/'
-import YYDropdown from 'yyzone/src/components/base/dropdown/'
-import YYTable from 'yyzone/src/components/base/table/'
-import YYDialog from 'yyzone/src/components/base/dialog/'
-import YYSwitch from 'yyzone/src/components/base/switch/'
-import YYInput from 'yyzone/src/components/base/input/'
 
 const options = { // 图片预览插件配置
     history: false,
@@ -73,35 +54,11 @@ getWebLang().then(lang => {
     i18n.locale = lang
     locale(lang)
 
-    Vue.component('YYAvatar', YYAvatar)
-    Vue.component('YYButton', YYButton)
-    Vue.component('YYDatePicker', YYDatePicker)
-    Vue.component('YYEmpty', YYEmpty)
-    Vue.component('YYLoading', YYLoading)
-    Vue.component('YYRadio', YYRadio)
-    Vue.component('YYRadioGroup', YYRadio.YYRadioGroup)
-    Vue.component('YYCheckbox', YYCheckbox)
-    Vue.component('YYCheckboxGroup', YYCheckbox.YYCheckboxGroup)
-    Vue.component('YYPagination', YYPagination)
-    Vue.component('YYLoadingH', YYLoadingH)
-    Vue.component('YYModal', YYModal)
-    Vue.component('YYSelect', YYSelect)
-    Vue.component('YYOption', YYSelect.YYOption)
-    Vue.component('YYDropdown', YYDropdown)
-    Vue.component('YYDropdownMenu', YYDropdown.YYDropdownMenu)
-    Vue.component('YYDropdownMenu', YYDropdown.YYDropdownMenu)
-    Vue.component('YYTable', YYTable)
-    Vue.component('YYDialog', YYDialog)
-    Vue.component('YYSwitch', YYSwitch)
-    Vue.component('YYInput', YYInput)
-
-    Vue.directive('yyloading', YYLoadingDirective)
-
+    Vue.use(yyzone, {
+        locale: lang
+    })
     Vue.prototype.$eventbus = new Vue() // 建立组件全局通信的钩子
     Vue.prototype.$ajax = ajax // 将ajax挂在到vue实例
-    Vue.prototype.$YYModal = YYModal
-    Vue.prototype.$YYLoading = YYLoading
-    Vue.prototype.$YYMessage = YYMessage
 
     const router = new VueRouter({ // 创建路由
         mode: 'hash',
