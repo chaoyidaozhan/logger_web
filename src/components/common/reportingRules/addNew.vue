fc9726d325427830fa09a0783da2209d5d5d73b7
dingyuc <dingyuc@yonyou.com>
2020-03-24 17:01:45 +0800

    2020-03-24 17:01

--- src/components/common/reportingRules/addNew.vue
+++ src/components/common/reportingRules/addNew.vue
@@ -313,16 +313,16 @@ export default {
 }
 </script>
 <style lang='less' scoped>
-  .moveR-enter-active,  .moveR-leave-active {
-    transition: all .1s linear;
-    transform: translateX(0);
-  }
-  .moveR-enter,  .moveR-leave {
-    transform: translateX(100%);
-  }
-  .moveR-leave-to{
-    transform: translateX(100%);
-  }
+  // .moveR-enter-active,  .moveR-leave-active {
+  //   transition: all .1s linear;
+  //   transform: translateX(0);
+  // }
+  // .moveR-enter,  .moveR-leave {
+  //   transform: translateX(100%);
+  // }
+  // .moveR-leave-to{
+  //   transform: translateX(100%);
+  // }
   .bgCover {
     position: fixed;
     right: 0;
