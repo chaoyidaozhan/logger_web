bd12668dbbfc259227dfe1af26b247189cac99fa
chaoyidaozhan <huangxiaogang123@aliyun.com>
2020-04-20 10:30:02 +0800

    优化工作汇报画图性能

--- src/components/business/query/globalModalitem.vue
+++ src/components/business/query/globalModalitem.vue
@@ -699,11 +699,14 @@ export default {
                 let pageLoggerList = document.querySelector('.page-logger-list')
                 let loggerItemModal = document.querySelector('.logger-item-modal')
                 let canvaldialog = _this.$el.querySelector('.canvaldialog')
-                canvaldialog.style.position = 'absolute'
-                canvaldialog.style.right = 0
-                canvaldialog.style.top = 0
-                canvaldialog.style.bottom = 0
-                canvaldialog.style.left = (loggerItemModal.offsetWidth - pageLoggerList.offsetWidth - 200)/2 + 'px'
+                //第一加载时算高宽，为了优化性能，以后都不要算了
+                if(!canvaldialog.getAttribute('width')) {
+                    canvaldialog.style.position = 'absolute'
+                    canvaldialog.style.right = 0
+                    canvaldialog.style.top = 0
+                    canvaldialog.style.bottom = 0
+                    canvaldialog.style.left = (loggerItemModal.offsetWidth - pageLoggerList.offsetWidth - 200)/2 + 'px'
+                }
 
                 canvas.width = pageLoggerList.offsetWidth
                 canvas.height = _this.$el.scrollHeight
